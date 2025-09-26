fed26c1580f4426f9a71b1d87a4aa778c8b2b736
Anastasia <nastya.il03@mail.ru>
2025-09-26 22:56:23 +0400

    add PostmanEchoTest

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -1,4 +1,20 @@
 package ru.netology.rest;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSentData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi") // что мы отправляем
+                .when().log().all()
+                .post("/post")
+                .then().log().all()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("Hi")); // проверка JSONPath
+    }
 }
